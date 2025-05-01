3be3e55e10c76758a9610fbf62446955695eba47
Titi7750 <tristanfio05@gmail.com>
2025-05-01 17:00:27 +0200

    [DEV]: Setting up logs file

---
+++ backend/logger.py
@@ -0,0 +1,23 @@
+import os
+import logging
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+log_file = os.path.join(LOG_DIR, "access.log")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler()
+    ]
+)
+
+def log_access(username: str, endpoint: str):
+
+    logging.info(f"User '{username}' accessed '{endpoint}'")
+
+def log_failed_login(username: str):
+
+    logging.warning(f"⚠ Failed login attempt for username: '{username}'")
--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException, Depends, Header
-from backend.models import UserLogin
-from backend.auth import verify_password, create_token, decode_token
 import duckdb
+from backend.models import UserLogin
+from backend.logger import log_access, log_failed_login
+from fastapi import FastAPI, HTTPException, Depends, Header
+from backend.auth import verify_password, create_token, decode_token
 
 app = FastAPI()
 duckdb.connect('city_bike.duckdb')
@@ -25,16 +26,20 @@ def login(user: UserLogin):
     with duckdb.connect("city_bike.duckdb") as connection:
         result = connection.execute("SELECT password FROM users WHERE username = ?", (user.username,)
     ).fetchone()
-
+        
     if result is None:
+        log_failed_login(user.username)
         raise HTTPException(status_code=401, detail="Invalid username")
 
     if not verify_password(user.password, result[0]):
         raise HTTPException(status_code=401, detail="Invalid password")
 
+    log_access(user.username, "/login")
     return {"token": create_token({"sub": user.username})}
 
 @app.get("/secure-data")
 def secure_data(token: dict = Depends(auth_required)):
+
+    log_access(token["sub"], "/secure-data")
 
     return {"message": f"Hello {token['sub']}, your data is safe!"}
